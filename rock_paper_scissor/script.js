1fb054471138d01692e2f44e1d5f85b136a9aae3
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-31 16:47:23 +0530

    add hide page section on btn click

--- rock_paper_scissor/script.js
+++ rock_paper_scissor/script.js
@@ -3,4 +3,21 @@
 const introSection = document.querySelector('.intro-page')
 const opponentSection = document.querySelector('.opponent-page')
 const detailSection = document.querySelector('.details-page')
+const playgroundSection = document.querySelector('.playground-page')
 
+const introBtn = document.querySelector('.intro-page button')
+const playBtn = document.querySelector('.details-page button')
+
+const hideSection = (...x) => {
+    x.forEach(el => el.style.display = 'none')
+}
+
+const logger = (msg) => {
+    console.log(msg)
+}
+
+introBtn.addEventListener('click', () => hideSection(introSection, detailSection, playgroundSection))
+playBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    hideSection(introSection,opponentSection,detailSection)
+})
