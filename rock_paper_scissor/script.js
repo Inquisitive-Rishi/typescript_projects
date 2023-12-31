37229ac7f11cdab90f2b552d67b4dd38843c94e2
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-31 16:27:49 +0530

    initial setup

---
+++ rock_paper_scissor/script.js
@@ -0,0 +1,6 @@
+// const em = '🪨 🧻 ✂️' 
+
+const introSection = document.querySelector('.intro-page')
+const opponentSection = document.querySelector('.opponent-page')
+const detailSection = document.querySelector('.details-page')
+
